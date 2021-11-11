d7a9a1612aadbeeb47cbbc1beeec5c638428a643
Jon Massey <jon.massey@thedatalab.org>
2021-11-11 15:20:46 +0000

    feat: github action plumbing

--- check.sh
+++ check.sh
@@ -8,34 +8,44 @@ allowed_datasets=$(
         sed -e "s/'//g"
 )
 
-declare -A FOUND_DATASETS
-matches_found=false
+
+declare -A MATCHED_FILES
+global_matches_found=false
 #echo $allowed_datasets
-for f in $(ls datasources | sed 's/.txt//g'); do
-    if [[ ",$allowed_datasets," = *",$f,"* ]]; then
-        continue
-    fi
-    matches=()
-    for rf in $(cat "datasources/$f.txt"); do
-        matches+=$(grep -n $rf $1 | grep "^[0-9]*\:[^#]")
+for study_def in $(find $1 -name "*.py"); do
+    declare -A FOUND_DATASETS
+    file_matches_found=false
+    for f in $(ls datasources | sed 's/.txt//g'); do
+        if [[ ",$allowed_datasets," = *",$f,"* ]]; then
+            continue
+        fi
+        matches=()
+        for rf in $(cat "datasources/$f.txt"); do
+            matches+=$(grep -n $rf $study_def | grep "^[0-9]*\:[^#]")
+        done
+        #add to dict if matches found
+        if [[ ${matches[@]} ]]; then
+            file_matches_found=true
+            FOUND_DATASETS["${f}"]=$matches
+        fi
     done
-    #add to dict if matches found
-    if [[ ${matches[@]} ]]; then
-        matches_found=true
-        FOUND_DATASETS["${f}"]=$matches
+    if "$file_matches_found" = true; then
+        global_matches_found=true
+        MATCHED_FILES["${study_def}"]=$FOUND_DATASETS
     fi
 done
 
-if "$matches_found" = true; then
-    echo "Instances of restricted function calls found:"
-    for key in "${!FOUND_DATASETS[@]}"; do
-        echo " - $key"
-        matches=${FOUND_DATASETS[$key]}
-        for match in "${matches[@]}"; do
-            echo -e "\t ${match}"
+if "$global_matches_found" = true; then
+    for study_def in "${!MATCHED_FILES[@]}"; do
+        echo "Instances of restricted function calls found in $study_def:"
+        for key in "${!FOUND_DATASETS[@]}"; do
+            echo " - $key"
+            matches=${FOUND_DATASETS[$key]}
+            for match in "${matches[@]}"; do
+                echo -e "\t ${match}"
+            done
         done
     done
-
     echo "If you believe this to be in error, please contact the OpenSAFELY Team"
     exit 1
 fi
--- test.sh
+++ test.sh
@@ -1,24 +1,32 @@
 #!/bin/bash
 export GITHUB_REPOSITORY="opensafely_dummy_icnarc"
+fail=false
 echo "testing: $GITHUB_REPOSITORY"
-if ! ./check.sh test/study_def_1.py; then
+if ! ./check.sh .; then
     echo "success"
 else
     echo "failure"
+    fail=true
 fi
 
 export GITHUB_REPOSITORY="opensafely_dummy_ons"
 echo "testing: $GITHUB_REPOSITORY"
-if ! ./check.sh test/study_def_1.py; then
+if ! ./check.sh .; then
     echo "success"
 else
     echo "failure"
+    fail=true
 fi
 
 export GITHUB_REPOSITORY="opensafely_dummy_icnarc_ons"
 echo "testing: $GITHUB_REPOSITORY"
-if ./check.sh test/study_def_1.py; then
+if ./check.sh .; then
     echo "success"
 else
     echo "failure"
+    fail=true
+fi
+
+if "$fail" = true; then
+    exit 1
 fi
